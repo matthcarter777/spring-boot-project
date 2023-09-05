8fbc747b16fd1f591e79eec1e7dc9c3a66554d5a
Mateus Henrique <mateushbsb@gmail.com>
2023-09-05 16:45:04 -0300

    Service layer, component registration

--- src/main/java/com/mateus/course/resources/UserResource.java
+++ src/main/java/com/mateus/course/resources/UserResource.java
@@ -1,20 +1,37 @@
 package com.mateus.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mateus.course.entities.User;
+import com.mateus.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service; 
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "mateus", "mateus@gmail.com", "619999", "2716371");
+	public ResponseEntity<List<User>> findAll() {
 		
-		return ResponseEntity.ok().body(u);
+		List<User> list =  service.findAll();
+		
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
+		
+		return ResponseEntity.ok().body(user);
+	}
+	
 }
---
+++ src/main/java/com/mateus/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.mateus.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateus.course.entities.User;
+import com.mateus.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.get();
+	}
+}
