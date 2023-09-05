4c0da7cb1dd29b376bc6e8785fe3788b4a4e758d
Mateus Henrique <mateushbsb@gmail.com>
2023-09-05 09:34:20 -0300

    User entity and resource

---
+++ src/main/java/com/mateus/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.mateus.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mateus.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "mateus", "mateus@gmail.com", "619999", "2716371");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
