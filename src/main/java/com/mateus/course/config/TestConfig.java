0f22f9e3e9e5dfc601144f9d5e060beb36b6504a
Mateus Henrique <mateushbsb@gmail.com>
2023-09-12 15:38:01 -0300

    Product

--- src/main/java/com/mateus/course/config/TestConfig.java
+++ src/main/java/com/mateus/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.mateus.course.entities.Category;
 import com.mateus.course.entities.Order;
+import com.mateus.course.entities.Product;
 import com.mateus.course.entities.User;
 import com.mateus.course.entities.enums.OrderStatus;
 import com.mateus.course.repositories.CategoryRepository;
 import com.mateus.course.repositories.OrderRepository;
+import com.mateus.course.repositories.ProductRepository;
 import com.mateus.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -49,7 +54,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
-
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 	}
 	
 	
