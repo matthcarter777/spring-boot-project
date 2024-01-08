f6a5de6d19fb4f998a97c0f050d4155acf1f54e1
mateus <mateushbsb@gmail.com>
2024-01-08 16:57:26 -0300

    user insert

--- src/main/java/com/mateus/course/services/UserService.java
+++ src/main/java/com/mateus/course/services/UserService.java
@@ -11,16 +11,20 @@ import com.mateus.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
 		return user.get();
+	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
 	}
 }
