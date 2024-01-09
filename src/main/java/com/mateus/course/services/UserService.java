88e3ed21ff889902369773c4b15ac041044ca875
mateus <mateushbsb@gmail.com>
2024-01-09 15:22:12 -0300

    Exception handling - findById

--- src/main/java/com/mateus/course/services/UserService.java
+++ src/main/java/com/mateus/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.mateus.course.entities.User;
 import com.mateus.course.repositories.UserRepository;
+import com.mateus.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
